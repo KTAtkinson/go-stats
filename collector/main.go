aeb48e23b203be3fbfaa2555048ac7ec698bc692
Katie <katie@atkinson.mn>
2018-12-29 18:09:20 -0800

    Add Flush method to OnDishFlusher

--- collector/main.go
+++ collector/main.go
@@ -1,12 +1,18 @@
 package collector
 
 import (
+	"fmt"
+	"path/filepath"
+	"os"
+	"log"
+	"io"
 	"sync"
+	"sort"
 	"time"
 )
 
 type Stat struct {
-	millis int64
+	time time.Time
 	value  int64
 }
 
@@ -66,8 +72,44 @@ type OnDiskFlusher struct {
 }
 
 // Flushes values to disk
-func (d *OnDiskFlusher) Flush(stats StatsRecorder) error {
-	return NOT_IMPLEMENTED
+func (d *OnDiskFlusher) Flush(stats StatsRecorder) (err error) {
+	for name, stat := range stats {
+		entries, err := stat.Reset()
+		if err != nil {
+			log.Printf("Error while resetting entry for %s.\n", name)
+			continue
+		}
+		sort.Slice(entries, func(i int, i1 int) bool { return entries[i].time.Before(entries[i1].time) })
+
+		var roundedTime time.Time
+		var writer io.WriteCloser
+		for _, evt := range entries {
+			roundedEventTime := evt.time.UTC().Round( time.Hour * 24 )
+			if roundedEventTime != roundedTime {
+				if writer != nil {
+					writer.Close()
+				}
+				fullPath := filepath.Join(d.flusherRoot, name, fmt.Sprintf("%d", roundedTime.Unix()))
+				fileMode := os.O_WRONLY|os.O_APPEND|os.O_CREATE
+				writer, err = os.OpenFile(fullPath, fileMode, 0644)
+				// This may mean the directory doesn't exist, so create it and see if the error goes away.
+				if os.IsNotExist(err) {
+					err = os.MkdirAll(filepath.Dir(fullPath), 0644)
+					if err != nil {
+						log.Printf("Failed to created new directory to record stats for %s.\n", name)
+						continue
+					}
+					writer, err = os.OpenFile(fullPath, fileMode, 0644)
+				}
+				if err != nil {
+					log.Printf("Not able to open file %s to record stats for %s.", fullPath, name)
+					continue
+				}
+			}
+			writer.Write([]byte( fmt.Sprintf("%d %d", evt.time.Unix(), evt.value) ))
+		}
+	}
+	return err
 }
 
 func NewOnDiskCollector(fp string) *Collector {
